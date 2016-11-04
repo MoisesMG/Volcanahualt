370fcfa89507e80f648bdafab22a77c3eaa1babb
Laleshka <laleskalopezklm@gmail.com>
2016-11-04 14:27:46 -0600

    prube

--- app/src/main/java/com/example/hp/volcanahualt/MainActivity.java
+++ app/src/main/java/com/example/hp/volcanahualt/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     public void llamar (View view){
         Intent i = new Intent(this, menu2.class);
         startActivity(i);
+
     }
 
 
